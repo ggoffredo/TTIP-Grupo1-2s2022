feb3edd4f737dbfb3565b65ab5e9a12410f8cbc8
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-08-31 17:39:49 -0300

    Rename gastos repository to gastos fijos repository

--- src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/repositories/GastosRepository.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unq.ttip.llegarafindemes.repositories
-
-import ar.edu.unq.ttip.llegarafindemes.models.gastos.Gasto
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface GastosRepository : JpaRepository<Gasto, Int>
