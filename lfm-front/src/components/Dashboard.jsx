e3f81ae409e51dd37c3b02fd3b98b49736331fde
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-09-02 20:09:02 -0300

    Remove await and encapsulate some data

--- lfm-front/src/components/Dashboard.jsx
+++ lfm-front/src/components/Dashboard.jsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
             url: 'http://localhost:8080/users/1/ingresos',
             responseType: 'stream',
         });
-        let ingresosApi = await response.data;
+        let ingresosApi = response.data;
         setIngresos(ingresosApi.reduce((partialSum, ingreso) => partialSum + ingreso.monto, 0));
     }
 
@@ -24,7 +24,7 @@ export default function Dashboard() {
             url: 'http://localhost:8080/users/1/gastos',
             responseType: 'stream',
         });
-        let gastosApi = await response.data;
+        let gastosApi = response.data;
         setGastos(gastosApi.reduce((partialSum, gasto) => partialSum + gasto.monto, 0));
     }
 
@@ -33,17 +33,23 @@ export default function Dashboard() {
         getGastos()
     }, []);
 
-    const chart = <DoughnutChart
-        data={{
+    function getDoughnutChartData() {
+        return {
             Gastos: gastos,
             Libre: ingresos - gastos
-        }}
-    />
+        };
+    }
+
+    function getDoughnutChart() {
+        return <DoughnutChart
+            data={getDoughnutChartData()}
+        />
+    }
 
     return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         <Grid item xs={2} sm={4} md={4}>
             <ChartCard
-                chart={chart}
+                chart={getDoughnutChart()}
                 label={`Ingresos: ${ingresos}`}
                 title={'Gastos vs ingresos'}
             />
