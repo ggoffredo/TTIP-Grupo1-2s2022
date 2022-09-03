aa478c1a38f4a5d8eacd021c3887b8155f2034ab
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-09-03 12:29:13 -0300

    Create GastosIngresosDoughnutChart

---
+++ lfm-front/src/components/CustomCharts/GastosIngresosDoughnutChart.jsx
@@ -0,0 +1,28 @@
+import ChartCard from "../ChartCard";
+import Grid from "@mui/material/Grid";
+import DoughnutChart from "../Core/Charts/DoughnutChart";
+
+const getDoughnutChartData = (ingresos, gastos) => {
+    return {
+        Gastos: gastos,
+        Libre: ingresos - gastos
+    };
+}
+
+const getDoughnutChart = (ingresos, gastos) => {
+    return <DoughnutChart
+        data={getDoughnutChartData(ingresos, gastos)}
+    />
+}
+
+const GastosIngresosDoughnutChart = ({ingresos, gastos}) => {
+    return <Grid item xs={2} sm={4} md={4}>
+        <ChartCard
+            chart={getDoughnutChart(ingresos, gastos)}
+            label={`Ingresos: ${ingresos}`}
+            title={'Gastos vs ingresos'}
+        />
+    </Grid>
+}
+
+export default GastosIngresosDoughnutChart
