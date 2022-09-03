80d8b59b8de70fe691cc620dff6a7b71562eec49
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-09-03 12:31:10 -0300

    Refactor Dashboard * Add use of services, and utils * Reduce amount of boilerplate code

--- lfm-front/src/components/Dashboard.jsx
+++ lfm-front/src/components/Dashboard.jsx
@@ -1,38 +1,29 @@
 import {useEffect, useState} from "react";
 import Grid from '@mui/material/Grid';
-import ChartCard from "./ChartCard";
-import axios from "axios";
-import DoughnutChart from "./Charts/DoughnutChart";
 import Divider from "@mui/material/Divider";
 import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from "@mui/material/Typography";
-import GastosTable from "./GastosTable";
+import GastosTable from "./CustomTables/GastosTable";
+import GastosIngresosDoughnutChart from "./CustomCharts/GastosIngresosDoughnutChart";
+import {arraySum} from '../helpers/Utils';
+import {getIngresosForUserId} from "../services/IngresosService";
+import {getGastosForUserId} from "../services/GastosService";
 
 export default function Dashboard() {
-    const [ingresos, setIngresos] = useState([])
     const [gastos, setGastos] = useState([])
     const [gastosAmount, setGastosAmount] = useState([])
+    const [ingresosAmount, setIngresosAmount] = useState([])
 
     async function getIngresos() {
-        let response = await axios({
-            method: 'get',
-            url: 'http://localhost:8080/users/1/ingresos',
-            responseType: 'stream',
-        });
-        let ingresosApi = response.data;
-        setIngresos(ingresosApi.reduce((partialSum, ingreso) => partialSum + ingreso.monto, 0));
+        let ingresosApi = await getIngresosForUserId(1);
+        setIngresosAmount(arraySum(ingresosApi, 'monto'));
     }
 
     async function getGastos() {
-        let response = await axios({
-            method: 'get',
-            url: 'http://localhost:8080/users/1/gastos',
-            responseType: 'stream',
-        });
-        let gastosApi = response.data;
+        let gastosApi = await getGastosForUserId(1);
         setGastos(gastosApi);
-        setGastosAmount(gastosApi.reduce((partialSum, gasto) => partialSum + gasto.monto, 0));
+        setGastosAmount(arraySum(gastosApi, 'monto'));
     }
 
     useEffect(() => {
@@ -40,47 +31,24 @@ export default function Dashboard() {
         getGastos()
     }, []);
 
-    function getDoughnutChartData() {
-        return {
-            Gastos: gastosAmount,
-            Libre: ingresos - gastosAmount
-        };
-    }
-
-    function getDoughnutChart() {
-        return <DoughnutChart
-            data={getDoughnutChartData()}
-        />
-    }
-
     return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         <Grid item xs={12}>
             <p style={{fontFamily: 'Staatliches', fontSize: '70px', marginTop: 0, marginBottom: 0, textAlign: 'left'}}>Dashboard</p>
             <Divider/>
         </Grid>
         <Grid item xs={12}>
+            {/*
+                TODO: Encapsular accordion como componente core. Deberia ser configurable el titulo, el spacing y deberia pasarse el array de componentes
+                a renderizar en los detalles
+            */}
             <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>} aria-controls="panel1a-content" id="panel1a-header">
                     <Typography>Gastos vs Ingresos</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Grid container item spacing={2}>
-                        <Grid item xs={2} sm={4} md={4}>
-                            <ChartCard
-                                chart={getDoughnutChart()}
-                                label={`Ingresos: ${ingresos}`}
-                                title={'Gastos vs ingresos'}
-                            />
-                        </Grid>
-                        <Grid item xs={2} sm={4} md={8}>
-                            <GastosTable
-                                data={gastos}
-                            />
-                        </Grid>
+                        <GastosIngresosDoughnutChart gastos={gastosAmount} ingresos={ingresosAmount}/>
+                        <GastosTable data={gastos}/>
                     </Grid>
                 </AccordionDetails>
             </Accordion>
