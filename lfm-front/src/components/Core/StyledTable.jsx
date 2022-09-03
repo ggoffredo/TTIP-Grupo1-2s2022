6d68fc06bddba2472d1334023c081fa4ba27005c
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-09-03 12:27:59 -0300

    Create core components concept * Move DoughnutChart to core components * Create StyleTable as core component

---
+++ lfm-front/src/components/Core/StyledTable.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+const TableHeadComponent = (headers) => (
+    <TableHead>
+        <TableRow>
+            {headers.map((header) => {
+                return <StyledTableCell align="center">{header.charAt(0).toUpperCase() + header.slice(1)}</StyledTableCell>
+            })}
+        </TableRow>
+    </TableHead>
+);
+
+const TableBodyComponent = (data, headers) => (
+    <TableBody>
+        {data.map((row) => (
+            <StyledTableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                {headers.map((header) => {
+                    return <StyledTableCell align="center">{row[header.toLowerCase()]}</StyledTableCell>
+                })}
+            </StyledTableRow>
+        ))}
+    </TableBody>
+);
+
+const StyledTable = ({data, headers}) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                {TableHeadComponent(headers)}
+                {TableBodyComponent(data, headers)}
+            </Table>
+        </TableContainer>
+    );
+}
+
+export default StyledTable;
