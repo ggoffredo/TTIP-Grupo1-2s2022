49de85c7409dc7262d402144b151d1feb3e96498
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-09-08 12:50:00 -0300

    Create DolarValues test * Create mock of AxiosHelper * Create mock responses * Modify App test to use new AxiosHelper mock

---
+++ lfm-front/src/helpers/__mocks__/DolarSiMockResponse.js
@@ -0,0 +1,20 @@
+export const dolares = [
+    {
+        "casa": {
+            "venta": "270,00",
+            "nombre": "Dolar Blue",
+        }
+    },
+    {
+        "casa": {
+            "venta": "272,250",
+            "nombre": "Dolar Bolsa",
+        }
+    },
+    {
+        "casa": {
+            "nombre": "Dolar turista",
+            "venta": "257,64",
+        }
+    }
+];
---
+++ lfm-front/src/helpers/__mocks__/GastosMockResponse.js
@@ -0,0 +1,74 @@
+export const gastos = [
+    {
+        "id": 1,
+        "descripcion": "Alquiler",
+        "monto": 30000,
+        "periodicidad": "MENSUAL",
+        "duracion": 0,
+        "fecha": "2022-09-01",
+        "medioDePago": {
+            "id": 1,
+            "nombre": "Efectivo"
+        }
+    },
+    {
+        "id": 2,
+        "descripcion": "Expensas",
+        "monto": 5000,
+        "periodicidad": "MENSUAL",
+        "duracion": 0,
+        "fecha": "2022-09-01",
+        "medioDePago": {
+            "id": 1,
+            "nombre": "Efectivo"
+        }
+    },
+    {
+        "id": 3,
+        "descripcion": "Telecentro",
+        "monto": 4000,
+        "periodicidad": "MENSUAL",
+        "duracion": 0,
+        "fecha": "2022-09-01",
+        "medioDePago": {
+            "id": 2,
+            "nombre": "Visa"
+        }
+    },
+    {
+        "id": 4,
+        "descripcion": "Gimnasio",
+        "monto": 4000,
+        "periodicidad": "MENSUAL",
+        "duracion": 0,
+        "fecha": "2022-09-01",
+        "medioDePago": {
+            "id": 2,
+            "nombre": "Visa"
+        }
+    },
+    {
+        "id": 13,
+        "descripcion": "Rappi",
+        "monto": 2500,
+        "periodicidad": "MENSUAL",
+        "duracion": 1,
+        "fecha": "2022-09-01",
+        "medioDePago": {
+            "id": 1,
+            "nombre": "Efectivo"
+        }
+    },
+    {
+        "id": 14,
+        "descripcion": "Pava Electrica",
+        "monto": 4000,
+        "periodicidad": "MENSUAL",
+        "duracion": 12,
+        "fecha": "2022-09-01",
+        "medioDePago": {
+            "id": 2,
+            "nombre": "Visa"
+        }
+    }
+];
