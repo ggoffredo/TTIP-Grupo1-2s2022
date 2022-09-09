38277305b8c5642a8119e49882d89822e47d5dc1
ggoffredo <gaston.goffredo@gmail.com>
2022-09-09 19:56:37 -0300

    Se crrige nombre de test

--- lfm-front/src/test/App.test.js
+++ lfm-front/src/test/App.test.js
@@ -4,7 +4,7 @@ import 'jest-canvas-mock';
 
 jest.mock('../helpers/AxiosHelper')
 
-test('renders learn react link', () => {
+test('Renders Gaston vs ingresos chart', () => {
   render(<App />);
   const linkElement = screen.getByText('Gastos vs ingresos');
   expect(linkElement).toBeInTheDocument();
