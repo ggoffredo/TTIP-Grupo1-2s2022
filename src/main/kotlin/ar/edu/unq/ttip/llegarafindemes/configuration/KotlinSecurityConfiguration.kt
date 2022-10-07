0ad459ef47a7a1512f8e9e929db8253f37abd9ea
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-10-07 15:40:05 -0300

    Decrease sonar code smells

--- src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/configuration/KotlinSecurityConfiguration.kt
+++ src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/configuration/KotlinSecurityConfiguration.kt
@@ -1,21 +1,23 @@
 package ar.edu.unq.ttip.llegarafindemes.configuration
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
 @EnableWebSecurity
-class KotlinSecurityConfiguration : WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity) {
+class KotlinSecurityConfiguration {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf()
             .disable()
             .authorizeRequests()
             .antMatchers("/**").permitAll()
             .anyRequest().authenticated()
+        return http.build()
     }
 }
