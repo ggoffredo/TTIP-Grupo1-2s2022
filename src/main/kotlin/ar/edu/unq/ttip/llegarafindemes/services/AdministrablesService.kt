e1b5e6b31392bd5d3901104d6d36735c3b0c3f5f
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-10-27 21:07:33 -0300

    Create date helper

--- src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/services/AdministrablesService.kt
+++ src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/services/AdministrablesService.kt
@@ -2,12 +2,10 @@ package ar.edu.unq.ttip.llegarafindemes.services
 
 import ar.edu.unq.ttip.llegarafindemes.dtos.GastosMensualizados
 import ar.edu.unq.ttip.llegarafindemes.dtos.IngresosMensualizados
+import ar.edu.unq.ttip.llegarafindemes.helpers.DateHelper
 import ar.edu.unq.ttip.llegarafindemes.models.Administrable
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.temporal.ChronoUnit
-import java.util.stream.Collectors
-import java.util.stream.Stream
 
 @Service
 class AdministrablesService {
@@ -20,12 +18,7 @@ class AdministrablesService {
         val fechaAdministrableMasViejo = this.administrables.first().fecha.withDayOfMonth(1)
         val fechaInicial = from ?: fechaAdministrableMasViejo
         val fechaFinal = to ?: LocalDate.now().plusMonths(1)
-        this.monthsToCalculate = Stream
-            .iterate(
-                fechaInicial
-            ) { date: LocalDate -> date.plusMonths(1) }
-            .limit(ChronoUnit.MONTHS.between(fechaInicial, fechaFinal))
-            .collect(Collectors.toList())
+        this.monthsToCalculate = DateHelper.getMonthsAsArray(fechaInicial, fechaFinal)
         return this
     }
 
--- src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/services/AhorrosService.kt
+++ src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/services/AhorrosService.kt
@@ -1,5 +1,6 @@
 package ar.edu.unq.ttip.llegarafindemes.services
 
+import ar.edu.unq.ttip.llegarafindemes.helpers.DateHelper
 import ar.edu.unq.ttip.llegarafindemes.models.Ahorro
 import org.springframework.stereotype.Service
 import java.time.LocalDate
@@ -9,15 +10,26 @@ class AhorrosService(private val gastosService: GastosService, private val ingre
 
     fun getAhorros(userId: Int, cantidadDeMeses: Int = 1): List<Ahorro> {
         val today = LocalDate.now().withDayOfMonth(1)
-        val ingresos = ingresosService.getIngresosForUserPerMonth(userId, today.minusMonths(cantidadDeMeses.toLong()), today.plusMonths(cantidadDeMeses.toLong()))
-        val gastos = gastosService.getGastosForUserPerMonth(userId, today.minusMonths(cantidadDeMeses.toLong()), today.plusMonths(cantidadDeMeses.toLong()))
+        val from = today.minusMonths(cantidadDeMeses.toLong())
+        val to = today.plusMonths(1)
+        val ingresos = ingresosService.getIngresosForUserPerMonth(userId, from, to)
+        val gastos = gastosService.getGastosForUserPerMonth(userId, from, to)
         // Si se persiste el valor en la db, acá podría obtenerse los ahorros para un mes determinado y partir de ahí
         var ahorroAcumulado = 0
-        return ingresos.zip(gastos).map {
+        var mesesConIngresosOGastos = 0
+        val ahorros = ingresos.zip(gastos).map {
                 (ingreso, gasto) ->
                     val ahorroActual = ingreso.montoTotal - gasto.montoTotal
+                    mesesConIngresosOGastos += if (ingreso.montoTotal != 0 || gasto.montoTotal != 0) 1 else 0
                     ahorroAcumulado += ahorroActual
                     Ahorro(ingreso.mes, ahorroActual, ahorroAcumulado)
+        }.toMutableList()
+        val promedioDeAhorros = ahorroAcumulado / mesesConIngresosOGastos
+        val ahorrosProyectados = DateHelper.getMonthsAsArray(to, to.plusMonths((cantidadDeMeses-1).toLong())).map {
+            ahorroAcumulado += promedioDeAhorros
+            Ahorro(it, promedioDeAhorros, ahorroAcumulado)
         }
+        ahorros.addAll(ahorrosProyectados)
+        return ahorros
     }
 }
