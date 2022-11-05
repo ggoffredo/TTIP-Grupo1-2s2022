03be4df259a67277bda088531fd9dcb41318b446
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-11-05 18:06:37 -0300

    Add plazo fijo uva investment logic * Add and format values to mock server

--- src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/services/AhorrosService.kt
+++ src/main/kotlin/ar/edu/unq/ttip/llegarafindemes/services/AhorrosService.kt
@@ -3,6 +3,7 @@ package ar.edu.unq.ttip.llegarafindemes.services
 import ar.edu.unq.ttip.llegarafindemes.exceptions.InvestmentNotFoundException
 import ar.edu.unq.ttip.llegarafindemes.helpers.DateHelper
 import ar.edu.unq.ttip.llegarafindemes.models.Ahorro
+import ar.edu.unq.ttip.llegarafindemes.models.Inversion
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -22,30 +23,72 @@ class AhorrosService(
         // Si se persiste el valor en la db, acá podría obtenerse los ahorros para un mes determinado y partir de ahí
         var ahorroAcumulado = 0
         var mesesConIngresosOGastos = 0
-        val ahorros = ingresos.zip(gastos).map {
+        val ahorros = mutableListOf<Ahorro>()
+        ingresos.zip(gastos).forEach {
                 (ingreso, gasto) ->
                     val ahorroActual = ingreso.montoTotal - gasto.montoTotal
                     mesesConIngresosOGastos += if (ingreso.montoTotal != 0 || gasto.montoTotal != 0) 1 else 0
                     ahorroAcumulado += ahorroActual
-                    Ahorro(ingreso.mes, ahorroActual, ahorroAcumulado)
-        }.toMutableList()
-        val promedioDeAhorros = ahorroAcumulado / mesesConIngresosOGastos
-        val ahorrosProyectados = DateHelper.getMonthsAsArray(to, to.plusMonths((cantidadDeMeses-1).toLong())).map {
-            ahorroAcumulado += promedioDeAhorros
-            Ahorro(it, promedioDeAhorros, ahorroAcumulado)
+                    ahorros.add(Ahorro(ingreso.mes, ahorroActual, ahorroAcumulado))
         }
-        ahorros.addAll(ahorrosProyectados)
+        val promedioDeAhorros = ahorroAcumulado / mesesConIngresosOGastos
+        DateHelper.getMonthsAsArray(to, to.plusMonths((cantidadDeMeses).toLong())).forEach {
+            ahorroAcumulado += promedioDeAhorros
+            ahorros.add(Ahorro(it, promedioDeAhorros, ahorroAcumulado))
+        }
         return ahorros
     }
 
-    fun getAhorrosConInversionAplicada(userId: Int, cantidadDeMeses: Int = 1, tipoDeInversion: String, nombreDeInversion: String): List<Ahorro> {
-        val inversiones = inversionesService.getInversiones().getOrElse(tipoDeInversion) { throw InvestmentNotFoundException() }
+    fun getAhorrosConInversionAplicada(userId: Int, cantidadDeMeses: Int = 1, tipoDeInversion: String, nombreDeInversion: String, invertirMesesFuturos: Boolean = true): List<Ahorro> {
+        val inversiones = inversionesService.getInversiones().values.flatten()
         val inversion = inversiones.find { it.nombre == nombreDeInversion } ?: throw InvestmentNotFoundException()
+        if (nombreDeInversion == "Plazo Fijo Uva") {
+            return this.getAhorrosConInversionUva(userId, cantidadDeMeses, inversion, invertirMesesFuturos)
+        }
+        return this.doGetAhorrosConInversionAplicada(userId, cantidadDeMeses, inversion, invertirMesesFuturos)
+    }
+
+    private fun doGetAhorrosConInversionAplicada(userId: Int, cantidadDeMeses: Int = 1, inversion: Inversion, invertirMesesFuturos: Boolean): List<Ahorro> {
         var ahorroAcumulado = 0
-        return this.getAhorros(userId, cantidadDeMeses).map {
+        val ahorrosInvertidos = mutableListOf<Ahorro>()
+        this.getAhorros(userId, cantidadDeMeses).forEach {
+            if (invertirMesesFuturos && it.fecha < LocalDate.now().withDayOfMonth(1)) {
+                ahorrosInvertidos.add(it)
+                return@forEach
+            }
             val nuevoAcumulado = (it.acumulado + ahorroAcumulado * (inversion.tasaDeVariacion / 100)).toInt()
             ahorroAcumulado += nuevoAcumulado
-            Ahorro(it.fecha, it.actual, nuevoAcumulado)
+            ahorrosInvertidos.add(Ahorro(it.fecha, it.actual, nuevoAcumulado))
         }
+        return ahorrosInvertidos
+    }
+
+    private fun getAhorrosConInversionUva(userId: Int, cantidadDeMeses: Int = 1, inversion: Inversion, invertirMesesFuturos: Boolean): List<Ahorro> {
+        var contadorMeses = 0
+        var intereses = 0
+        var agregarIntereses = false
+        val ahorrosInvertidos = mutableListOf<Ahorro>()
+        this.getAhorros(userId, cantidadDeMeses).forEach {
+            if (invertirMesesFuturos && it.fecha < LocalDate.now().withDayOfMonth(1)) {
+                ahorrosInvertidos.add(it)
+                return@forEach
+            }
+            if (contadorMeses == 0) {
+                contadorMeses = 2
+                if (agregarIntereses) {
+                    val ahorro = Ahorro(it.fecha, it.actual, it.acumulado + intereses)
+                    ahorrosInvertidos.add(ahorro)
+                    intereses = (ahorro.acumulado * (inversion.tasaDeVariacion * 3) / 100).toInt()
+                } else {
+                    intereses = (it.acumulado * (inversion.tasaDeVariacion * 3) / 100).toInt()
+                    ahorrosInvertidos.add(it)
+                }
+                agregarIntereses = true
+            } else {
+                ahorrosInvertidos.add(it)
+                contadorMeses -= 1
+            }
+        }
+        return ahorrosInvertidos
     }
 }
